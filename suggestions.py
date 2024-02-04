f3fbf51bcd1258480ce715595f77f125c338992d
Ivan Maslov <ivan.maslov99@gmail.com>
2024-02-04 22:42:14 +0400

    cleaned code

--- suggestions.py
+++ suggestions.py
@@ -2,22 +2,17 @@ from sentence_transformers import util, SentenceTransformer
 from typing import List, Dict, Tuple
 from spacy.tokens import Doc
 
-def calculate_similarity(
-    input_phrase: List[str], 
-    standard_terms: List[str], 
-    model: SentenceTransformer
-) -> Tuple[str, float]:
-  """
-  Calculates the cosine similarity between input_phrase and standard_terms.
-  
-  Returns a tuple of the most similar standard term and its score.
-  """
-  embeddings1 = model.encode(input_phrase, convert_to_tensor=True)
-  embeddings2 = model.encode(standard_terms, convert_to_tensor=True)
-  similarity_scores = util.pytorch_cos_sim(embeddings1, embeddings2).flatten()
-  best_id = similarity_scores.argmax()
-  return standard_terms[best_id], similarity_scores[best_id]
-
+def calculate_similarity(input_phrases: List[str], standard_terms: List[str], model: SentenceTransformer) -> Tuple[str, float]:
+    """
+    Calculates the cosine similarity between input_phrases and standard_terms.
+    
+    Returns a tuple of the most similar standard term and its score.
+    """
+    input_embeddings = model.encode(input_phrases, convert_to_tensor=True)
+    standard_embeddings = model.encode(standard_terms, convert_to_tensor=True)
+    similarity_scores = util.pytorch_cos_sim(input_embeddings, standard_embeddings).flatten()
+    most_similar_index = similarity_scores.argmax()
+    return standard_terms[most_similar_index], similarity_scores[most_similar_index]
 
 def generate_suggestions(
   input_text: str,
@@ -25,7 +20,7 @@ def generate_suggestions(
   model: SentenceTransformer,
   nlp: Doc,
   threshold: float = 0.45
-) -> Dict[str, List[Tuple[str, float]]]:
+) -> Dict[str, Tuple[str, float]]:
   """
   Generates replacement suggestions for input_text 
   based on the similarity between certain phrases in this text and standard_terms.
@@ -38,17 +33,13 @@ def generate_suggestions(
   suggestions = {}
   for token in doc:
     subtree_text = ' '.join(n.text for n in token.subtree)
-    for input_phrase in set([token.text, subtree_text]):
-      # Form a chunk with varying lengths
+    phrases_to_check = set([token.text, subtree_text])
+    for input_phrase in phrases_to_check:
       best_term, similarity_score = calculate_similarity([input_phrase], standard_terms, model)
-      
       if similarity_score > threshold:
         if input_phrase in suggestions:
-          suggestions[input_phrase] += [(best_term, similarity_score)]
+          suggestions[input_phrase] = max(suggestions[input_phrase], (best_term, similarity_score), key=lambda x: x[1])
         else:
-          suggestions.update({input_phrase: [(best_term, similarity_score)]})
-
-  for key, value in suggestions.items():
-    suggestions[key] = sorted(value, key=lambda x: x[1], reverse=True)[0]
+          suggestions[input_phrase] = (best_term, similarity_score)
 
   return suggestions
